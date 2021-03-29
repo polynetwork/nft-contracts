3f16e35a832845d40958920e286a27c131a6e3d9
dylenfu <dylenfu@126.com>
2021-03-29 11:33:00 +0800

    init

---
+++ contracts_nftlockproxy/PolyNFTWrap.sol
@@ -0,0 +1,105 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity >=0.5.0;
+
+import "./libs/ownership/Ownable.sol";
+import "./libs/token/ERC20/SafeERC20.sol";
+import "./libs/math/SafeMath.sol";
+import "./libs/lifecycle/Pausable.sol";
+import "./libs/common/ZeroCopySink.sol";
+import "./libs/token/ERC721/IERC721.sol";
+import "./libs/utils/ReentrancyGuard.sol";
+import "./interface/INFTLockProxy.sol";
+
+contract PolyNFTWrapper is Ownable, Pausable, ReentrancyGuard {
+    using SafeMath for uint;
+    using SafeERC20 for IERC20;
+
+    uint public chainId;
+    address public feeCollector;
+
+    INFTLockProxy public lockProxy;
+
+    struct CallArgs {
+        bytes toAddress;
+        uint64 toChainId;
+    }
+
+    event PolyWrapperLock(address indexed fromAsset, address indexed sender, uint64 toChainId, address toAddress, uint256 tokenId, uint256 fee, uint id);
+    event PolyWrapperSpeedUp(address indexed fromAsset, bytes indexed txHash, address indexed sender, uint256 efee);
+
+    constructor(address _owner, uint _chainId) public {
+        require(_chainId != 0, "!legal");
+        transferOwnership(_owner);
+        chainId = _chainId;
+    }
+    
+    function setFeeCollector(address collector) external onlyOwner {
+        require(collector != address(0), "emtpy address");
+        feeCollector = collector;
+    }
+
+    function setLockProxy(address _lockProxy) external onlyOwner {
+        require(_lockProxy != address(0));
+        lockProxy = INFTLockProxy(_lockProxy);
+        require(lockProxy.managerProxyContract() != address(0), "not lockproxy");
+    }
+
+    function pause() external onlyOwner {
+        _pause();
+    }
+
+    function unpause() external onlyOwner {
+        _unpause();
+    }
+
+    function extractFee(address token) external {
+        require(msg.sender == feeCollector, "!feeCollector");
+        if (token == address(0)) {
+            payable(msg.sender).transfer(address(this).balance);
+        } else {
+            IERC20(token).safeTransfer(feeCollector, IERC20(token).balanceOf(address(this)));
+        }
+    }
+
+    function lock(address fromAsset, uint64 toChainId, address toAddress, uint256 tokenId, uint256 fee, uint id) external payable nonReentrant whenNotPaused {    
+        require(toChainId != chainId && toChainId != 0, "!toChainId");
+        _pull(fromAsset, fee);
+        _push(fromAsset, toChainId, toAddress, tokenId);
+        emit PolyWrapperLock(fromAsset, msg.sender, toChainId, toAddress, tokenId, fee, id);
+    }
+
+    function speedUp(address fromAsset, bytes memory txHash, uint256 fee) external payable nonReentrant whenNotPaused {
+        _pull(fromAsset, fee);
+        emit PolyWrapperSpeedUp(fromAsset, txHash, msg.sender, fee);
+    }
+
+    function _pull(address fromAsset, uint256 fee) internal {
+        if (fromAsset == address(0)) {
+            require(msg.value == fee, "insufficient ether");
+        } else {
+            IERC20(fromAsset).safeTransferFrom(msg.sender, address(this), fee);
+        }
+    }
+
+    function _push(address fromAsset, uint64 toChainId, address toAddress, uint256 tokenId) internal {
+        IERC721 nftContract = IERC721(fromAsset);
+        nftContract.approve(address(lockProxy), tokenId);
+
+        CallArgs memory callArgs = CallArgs({
+            toAddress: abi.encodePacked(toAddress),
+            toChainId: toChainId
+        });
+        bytes memory callData = _serializeCallArgs(callArgs);
+        nftContract.safeTransferFrom(msg.sender, toAddress, tokenId, callData);
+    }
+
+    function _serializeCallArgs(CallArgs memory args) internal pure returns (bytes memory) {
+        bytes memory buff;
+        buff = abi.encodePacked(
+            ZeroCopySink.WriteVarBytes(args.toAddress),
+            ZeroCopySink.WriteUint64(args.toChainId)
+            );
+        return buff;
+    }
+}
---
+++ go_abi/eccd_abi/eccd_abi.go
@@ -0,0 +1,8 @@
+Fatal: Failed to build Solidity contract: solc: exit status 1
+Error: Source file requires different compiler version (current compiler is 0.6.12+commit.27d51765.Darwin.appleclang) - note that nightly builds are considered to be strictly less than the released version
+ --> contracts/core/cross_chain_manager/data/EthCrossChainData.sol:1:1:
+  |
+1 | pragma solidity ^0.5.0;
+  | ^^^^^^^^^^^^^^^^^^^^^^^
+
+
