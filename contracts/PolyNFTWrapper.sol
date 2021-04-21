97b101f16d627a1f45fa43c619cb91f7fdcde8a3
dylenfu <dylenfu@126.com>
2021-04-21 14:31:41 +0800

    getUnCrossChainNFT tokens

--- contracts/PolyNFTWrapper.sol
+++ contracts/PolyNFTWrapper.sol
@@ -64,63 +64,89 @@ contract PolyNFTWrapper is Ownable, Pausable, ReentrancyGuard {
         }
     }
 
-    function getAndCheckTokenUrl(address asset, address user, uint tokenId) public view returns (string memory) {
+    function getAndCheckTokenUrl(address asset, address user, uint tokenId) public view returns (bool, string memory) {
+        string memory url = "";
         address owner = IERC721(asset).ownerOf(tokenId);
-        require(address(user) == address(owner), "user is not token's owner");
+        if (user != owner || user == address(0)) {
+            return (false, url);
+        }
 
-        string memory url = IERC721Metadata(asset).tokenURI(tokenId);
-        return url;
+        url = IERC721Metadata(asset).tokenURI(tokenId);
+        return (true, url);
     }
 
     // getTokensByIndex index start from 0
-    function getTokensByIds(address asset, bytes calldata args) public view returns (bytes memory) {
+    function getOwnerTokensByIndex(address asset, address owner, uint start, uint length) public view returns (bool, bytes memory) {
+        bytes memory buff;
+        if (length == 0 || length > 10) {
+            return (false, buff);
+        }
+
+        uint total = IERC721(asset).balanceOf(owner);
+        if (total == 0 || start >= total) {
+            return (false, buff);
+        }        
+        uint end = _calcEndIndex(start, length, total);
+
+        IERC721Metadata meta = IERC721Metadata(asset);
+        IERC721Enumerable enu = IERC721Enumerable(asset);
+        for (uint index = start; index <= end; index++) {
+            uint tokenId = enu.tokenOfOwnerByIndex(owner, index);
+            string memory url = meta.tokenURI(tokenId);
+            buff = _serializeProfile(buff, tokenId, url);
+        }
+        return (true, buff);
+    }
+
+    // getTokensByIndex index start from 0
+    function getTokensByIds(address asset, bytes calldata args) public view returns (bool, bytes memory) {
         uint off = 0;
         uint tokenId = 0;
         uint length = 0;
         bytes memory buff;
 
         (length, off) = ZeroCopySource.NextUint256(args, off);
-        require(length > 0 && length <= 10, "length out of range");
+        if (length == 0 || length > 10) {
+            return (false, buff);
+        }
 
         IERC721Metadata meta = IERC721Metadata(asset);
         for (uint index = 0; index < length; index++) {
             (tokenId, off) = ZeroCopySource.NextUint256(args, off);
             string memory url = meta.tokenURI(tokenId);
-            buff = abi.encodePacked(
-                buff,
-                ZeroCopySink.WriteUint256(tokenId),
-                ZeroCopySink.WriteVarBytes(bytes(url))
-            );
+            buff = _serializeProfile(buff, tokenId, url);
         }
-        
-        return buff;
+        return (true, buff);
     }
 
-    // getTokensByIndex index start from 0
-    function getTokensByIndex(address asset, address owner, uint start, uint length) public view returns (bytes memory) {
-        require(length > 0 && length <= 10, "length out of range");
-
-        uint total = IERC721(asset).balanceOf(owner);
-        require(total > 0, "total balance is zero");
-
-        uint end = start + length - 1;
-        if (end >= total) {
-            end = total - 1;
-        }
-        
+    function getUnCrossChainTokensByIndex(address asset, uint start, uint length) public view returns (bool, bytes memory) {
         bytes memory buff;
+        if (length == 0 || length > 10) {
+            return (false, buff);
+        }
+
         IERC721Metadata meta = IERC721Metadata(asset);
         IERC721Enumerable enu = IERC721Enumerable(asset);
-        for (uint index = start; index <= end; index++) {
-            uint tokenId = enu.tokenOfOwnerByIndex(owner, index);
-            string memory url = meta.tokenURI(tokenId);
-            buff = abi.encodePacked(
-                buff,
-                ZeroCopySink.WriteUint256(tokenId),
-                ZeroCopySink.WriteVarBytes(bytes(url))
-            );
+        IERC721 erc = IERC721(asset);
+        
+        uint256 total = enu.totalSupply();
+        if (total == 0 || start >= total) {
+            return (false, buff);
         }
-        return buff;
+
+        uint end = _calcEndIndex(start, length, total);
+        while(start <= end && end < total) {
+            uint tokenId = enu.tokenByIndex(start);
+            start = start + 1;
+            address owner = erc.ownerOf(tokenId);
+            if (owner == lockProxy) {
+                end = end + 1;
+                continue;
+            }
+            string memory url = meta.tokenURI(tokenId);
+            buff = _serializeProfile(buff, tokenId, url);
+        }
+        return (true, buff);
     }
 
     function lock(address fromAsset, uint64 toChainId, address toAddress, uint256 tokenId, address feeToken, uint256 fee, uint id) external payable nonReentrant whenNotPaused {    
@@ -160,5 +186,22 @@ contract PolyNFTWrapper is Ownable, Pausable, ReentrancyGuard {
             ZeroCopySink.WriteUint64(args.toChainId)
             );
         return buff;
+    }
+
+    function _serializeProfile(bytes memory buff, uint tokenId, string memory url) internal pure returns (bytes memory) {
+        buff = abi.encodePacked(
+            buff,
+            ZeroCopySink.WriteUint256(tokenId),
+            ZeroCopySink.WriteVarBytes(bytes(url))
+        );
+        return buff;
+    }
+
+    function _calcEndIndex(uint start, uint length, uint total) internal pure returns (uint) {
+        uint end = start + length - 1;
+        if (end >= total) {
+            end = total - 1;
+        }
+        return end;
     }
 }
---
+++ load_remix.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+remixd -s ~/software/nft-bridge/nft-contracts --remix-ide https://remix.ethereum.org
